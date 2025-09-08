97b4957f32b2996ee31b5ceb2a29f993313d2f18
sai <knvsai.9@gmail.com>
2025-09-08 13:27:45 +0530

    Initial commit

---
+++ myfirstreactapp/src/Footer.jsx
@@ -0,0 +1,6 @@
+function Footer(){
+    return <>
+    <h1 style={{color:'blue', backgroundColor:'lightgrey'}}>My Footer</h1>
+    </>
+export default Footer;
+}                                                                                                               
