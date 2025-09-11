7acb898daf5146caf3108659b28c1d8339797f45
sai <knvsai.9@gmail.com>
2025-09-11 09:59:55 +0530

    Updation1

--- myfirstreactapp/src/Footer.jsx
+++
@@ -1,6 +0,0 @@
-function Footer(){
-    return <>
-    <h1 style={{color:'blue', backgroundColor:'lightgrey'}}>My Footer</h1>
-    </>
-export default Footer;
-}                                                                                                               
--- myfirstreactapp/src/MyHeader.jsx
+++
@@ -1,4 +0,0 @@
-function MyHeader(){
-  return <><h1>My Header</h1></>
-}
-export default MyHeader;
