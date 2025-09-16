92f20fd8878f07951406d396d3ba5c30ff3a86a1
sai <knvsai.9@gmail.com>
2025-09-16 14:54:03 +0530

    myfirstreactapp update 2

--- myfirstreactapp/src/App.jsx
+++ myfirstreactapp/src/App.jsx
@@ -1,12 +1,16 @@
-import './App.css'
-import StudentComponent from './Components/StudentComponent'
-
 function App(){
-  return(
-    <>
-    <h1>Welcome to React</h1>
-    <StudentComponent/>
-    </>
-  )
+  const numbers=[1,2,3,4,5,6]
+
+  const squares=numbers.map((ele)=>Math.pow(ele,2))
+  const cubes = numbers.map((ele)=>Math.pow(ele,3))
+  const evenNum=numbers.filter((ele)=>ele%2===0)
+  const oddNum=numbers.filter((ele)=>ele%2===1)
+  const total=numbers.reduce((acc,ele)=>acc+ele)
+
+  console.log(squares)
+  console.log(cubes)
+  console.log(evenNum)
+  console.log(oddNum)
+  console.log(total)
 }
-export default App
+export default App;
