63ecc2fc7f7b9622f53b8610c468a43d37c05e66
sai <knvsai.9@gmail.com>
2025-09-17 14:59:40 +0530

    myfirstreactappupdate2

--- myfirstreactapp/src/App.jsx
+++ myfirstreactapp/src/App.jsx
@@ -1,16 +1,11 @@
-function App(){
-  const numbers=[1,2,3,4,5,6]
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import Routing from "./Routing"; // make sure the file is src/Routing.jsx
 
-  const squares=numbers.map((ele)=>Math.pow(ele,2))
-  const cubes = numbers.map((ele)=>Math.pow(ele,3))
-  const evenNum=numbers.filter((ele)=>ele%2===0)
-  const oddNum=numbers.filter((ele)=>ele%2===1)
-  const total=numbers.reduce((acc,ele)=>acc+ele)
-
-  console.log(squares)
-  console.log(cubes)
-  console.log(evenNum)
-  console.log(oddNum)
-  console.log(total)
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routing />
+    </BrowserRouter>
+  );
 }
-export default App;
