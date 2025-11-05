e62748c51438e5bf6ca387be9e907658243a3e2d
sai <knvsai.9@gmail.com>
2025-11-05 15:07:50 +0530

    FSVSedev1

--- FullStackApp/backend/server.js
+++ FullStackApp/backend/server.js
@@ -1,11 +1,40 @@
-const express=require('express');
-const app=express();
-const port = 5000;
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+const PORT = 5000;
 
-app.get('/api/message',(req,res)=>{
-    res.json({message:"Hello from Express Backend!"});
+// Allow JSON requests
+app.use(express.json());
+
+// 1) Connect to MongoDB
+mongoose.connect("mongodb://localhost:27017/", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log("MongoDB Connected"))
+.catch(err => console.log("MongoDB Connection Error:", err));
+
+const PostSchema = new mongoose.Schema({
+  title: String,
+  content: String,
+});
+const Post = mongoose.model("Post", PostSchema);
+app.get('/api/message', (req, res) => {
+  res.json({ message: "Hello from Express Backend with MongoDB! " });
 });
 
-app.listen(port, ()=> {
-    console.log(`Express server running at https://localhost:${port}`);
+// Create a post
+app.post('/api/posts', async (req, res) => {
+  const post = await Post.create(req.body);
+  res.json(post);
+});
+
+// Get all posts
+app.get('/api/posts', async (req, res) => {
+  const posts = await Post.find();
+  res.json(posts);
+});
+
+app.listen(PORT, () => {
+  console.log(`Backend running at http://localhost:${PORT}`);
 });
